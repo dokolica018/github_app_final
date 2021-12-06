f27f9939bc5809b24ed9d9bb62582292fbbff599
Andreja Stanimirovic <andrejastanimirovic@gmail.com>
2021-12-06 17:46:43 +0100

    Update service.js

--- src/service.js
+++ src/service.js
@@ -1,6 +1,5 @@
 import axios from "axios";
 
-export const getGithubProfile = (user) => axios.get(`https://api.github.com/users/${user}`,{
-})
+export const getGithubProfile = (user) => axios.get(`https://api.github.com/users/${user}`)
 export const getGithubRepos = (user) => axios.get(`https://api.github.com/users/${user}/repos`)
 export const getGithubFollowers = (user) => axios.get(`https://api.github.com/users/${user}/followers`)
